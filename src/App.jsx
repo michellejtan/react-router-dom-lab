70459e29a8c6a5acafb899ed1d784f4ea3199b8d
michellejtan <michelle0223@gmail.com>
2025-03-31 19:51:51 -0700

    wip: refactored route config; added new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
 import NavBar from './components/NavBar/NavBar';
+import { Routes, Route } from 'react-router';
 import './index.css'
 import './App.css'
 
@@ -8,6 +12,22 @@ const App = () => {
     <NavBar />
     <h1>Mailbox List</h1>
 
+    <Routes>
+         {/* <PokemonList pokemon={pokemon} /> */}
+         <Route path="/"
+           element={<main><h1>Post Office</h1></main>} />
+         <Route path="/mailboxes"
+           element={<MailboxList />} />
+         <Route
+           path="/new-mailbox"
+           element={<MailboxForm/>}
+         />
+         <Route
+           path="/mailboxes/:mailboxId"
+           element={<MailboxDetails />}
+         />
+         <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+       </Routes>
   </>
   );
 };
