fbc7e13275d7ef73f23b891ccc537ee12aad8871
michellejtan <michelle0223@gmail.com>
2025-04-01 00:11:15 -0700

    wip: create a new useState variable - mailboxes & Set initial state to an empty array

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,41 @@ import MailboxList from './components/MailboxList/MailboxList';
 import NavBar from './components/NavBar/NavBar';
 import { Routes, Route } from 'react-router';
 import './index.css'
-import './App.css'
+// import './App.css'
+
+;
+// const initialState = [
+//   {
+//     _id: 1,
+//     boxSize: 'Small',
+//     boxOwner: 'Alex',
+//   }
+// ];
 
 const App = () => {
-  return (<>
-    <NavBar />
-    <h1>Mailbox List</h1>
 
-    <Routes>
-         {/* <PokemonList pokemon={pokemon} /> */}
-         <Route path="/"
-           element={<main><h1>Post Office</h1></main>} />
-         <Route path="/mailboxes"
-           element={<MailboxList />} />
-         <Route
-           path="/new-mailbox"
-           element={<MailboxForm/>}
-         />
-         <Route
-           path="/mailboxes/:mailboxId"
-           element={<MailboxDetails />}
-         />
-         <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
-       </Routes>
-  </>
+  const [mailboxes, setMailBoxes] = useState([]);
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/"
+          element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />} />
+        <Route
+          path="/new-mailbox"
+          element={<MailboxForm />}
+        />
+        <Route
+          path="/mailboxes/:mailboxId"
+          element={<MailboxDetails />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
+    </>
   );
 };
 
-export default App
+export default App;
