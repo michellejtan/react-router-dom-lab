2c7574265ed6a14fe7fbfcdf8592c820bb133809
michellejtan <michelle0223@gmail.com>
2025-04-02 08:20:52 -0700

    wip: display a list of all the letters for selected mail box & message when selectedBox  doesn't even exist

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -6,6 +6,15 @@ const MailboxDetails = (props) => {
         mailbox._id === Number(mailboxId)
     ));
 
+    const selectedLetters = props.letters.filter((letter) => (
+        letter.mailboxId === mailboxId
+
+    ));
+    // console.log(selectedLetters);
+
+    if (!selectedBox || props.selectedBox.length === 0) //selectedBox doesn't even exist!
+        return <h1>No mailbox Found</h1>;
+    
     return (
         <>
             <h1>Mailbox {mailboxId}</h1>
@@ -13,6 +22,21 @@ const MailboxDetails = (props) => {
 
             <p>Boxholder: {selectedBox.boxOwner}</p>
             <p>Box Size: {selectedBox.boxSize}</p>
+
+            <h2>Letters</h2>
+            {selectedLetters.length === 0 ? (
+                <p>No letters</p>
+            ) : (
+                selectedLetters.map((letter, index) => {
+                    return (
+                        <div key={index}>
+                            <p>Dear {letter.recipient},</p>
+                            <p>{letter.message}</p>
+                        </div>
+                    );
+                })
+
+            )}
         </>
     );
 };
